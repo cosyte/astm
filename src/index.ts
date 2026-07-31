93c8aa5a87e3255e9f8d39936d166d9b5ea1753b
Noah Schatz <noah.lane.schatz@gmail.com>
2026-07-30 20:16:58 -0400

    Version Packages (#29)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,7 @@
  * console.log(VERSION);
  * ```
  */
-export const VERSION: string = "0.0.5";
+export const VERSION: string = "0.0.6";
 
 export { parseAstmRecords, AstmStrictError, attachComments } from "./records/parse.js";
 export {
